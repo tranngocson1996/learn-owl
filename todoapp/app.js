3b7f52b9a03ee9491cd07708b5d78e7d90017b80
Mountain Tran <tranngocson8296@gmail.com>
2023-03-17 22:22:39 +0700

    3. Displaying a list of tasks-OWL Tutorial: TodoApp 4. Layout: some basic css-OWL Tutorial: TodoApp

--- todoapp/app.js
+++ todoapp/app.js
@@ -6,17 +6,44 @@ const { Component, mount, xml } = owl;
 
 // Owl Components
 class Root extends Component {
-// Đoạn code static template = xml<div>todo app</div>; được sử dụng để định nghĩa một mẫu tĩnh cho một thành phần OWL. Mẫu tĩnh sẽ không thay đổi dựa trên trạng thái hoặc dữ liệu của thành phần.
-// Trong khi đó, App.template = xml<div>todo app</div>; được sử dụng để định nghĩa một mẫu động cho thành phần App trong OWL. Mẫu động có thể được cập nhật dựa trên trạng thái và dữ liệu của thành phần.
-// Vì vậy, khi sử dụng static template, mẫu được xác định sẽ không thể thay đổi, trong khi sử dụng App.template, mẫu có thể được cập nhật để phản ánh các thay đổi trong trạng thái hoặc dữ liệu của thành phần.
-//  static template = xml`<div>todo app</div>`;
-//  App.template = xml<div>todo app</div>;
 
-// Cả hai câu lệnh đều được sử dụng để tạo một mẫu tĩnh (static template) trong OWL. Tuy nhiên, có một sự khác biệt nhỏ giữa chúng.
-// Trong trường hợp static template = xml /* xml */<div>todo app</div>;, câu lệnh sử dụng cú pháp template string (``) để định nghĩa một chuỗi có chứa một phần tử HTML. Ký tự /* xml */ trước chuỗi là một comment, không ảnh hưởng đến giá trị của chuỗi.
-// Trong khi đó, trong trường hợp static template = xml<div>todo app</div>;, câu lệnh đơn giản chỉ định một chuỗi có chứa một phần tử HTML mà không sử dụng cú pháp template string.
-// Tóm lại, cả hai câu lệnh đều tạo ra một mẫu tĩnh (static template) trong OWL, tuy nhiên, cú pháp của chúng khác nhau.
-    static template = xml /* xml */`<div>todo app</div>`;
+    // Đoạn mã này định nghĩa một mẫu OWL để hiển thị danh sách các công việc (tasks) dưới dạng các phần tử HTML. Mẫu này sử dụng các đặc tính của OWL để tạo ra các phần tử HTML dựa trên các đối tượng trong mảng tasks.
+    // Cụ thể, các phần tử HTML được tạo ra bao gồm:
+    //     Một phần tử <div> có class là task-list, đại diện cho danh sách các công việc.
+    //     Một phần tử <t> được sử dụng để lặp qua các đối tượng trong mảng tasks.
+    //     Trong mỗi vòng lặp, một phần tử <div> có class là task được tạo ra, đại diện cho một công việc.
+    //     Trong phần tử <div> của mỗi công việc, một phần tử <input> kiểu checkbox được tạo ra, với thuộc tính checked được thiết lập bằng thuộc tính t-att-checked="task.isCompleted", đại diện cho trạng thái hoàn thành của công việc.
+    //     Ngoài ra, trong phần tử <div> của mỗi công việc còn có một phần tử <span> chứa nội dung của công việc, được tạo ra bằng cú pháp t-esc="task.text", đại diện cho nội dung của công việc.
+    // Trong tổng thể, mẫu OWL này sử dụng các đặc tính của OWL để tạo ra các phần tử HTML dựa trên các đối tượng trong mảng tasks, giúp cho việc hiển thị danh sách các công việc trở nên dễ dàng hơn.
+    static template = xml/* xml */ `
+    <div class="task-list">
+        <t t-foreach="tasks" t-as="task" t-key="task.id">
+            <div class="task" t-att-class="task.isCompleted ? 'done' : ''">
+                <input type="checkbox" t-att-checked="task.isCompleted"/>
+                <span><t t-esc="task.text"/></span>
+            </div>
+        </t>
+    </div>`;
+    // Mẫu này chứa một vòng lặp t-foreach để lặp qua các công việc trong mảng tasks. Nó có thể tìm thấy danh sách tasks từ thành phần (component) vì ngữ cảnh (context) của việc render chứa các thuộc tính của thành phần đó. Lưu ý rằng chúng ta sử dụng id của mỗi công việc làm t-key, điều này rất phổ biến. Có hai lớp css: task-list và task, mà chúng ta sẽ sử dụng trong phần tiếp theo.
+    // Cuối cùng, hãy chú ý đến việc sử dụng thuộc tính t-att-checked: tiền tố một thuộc tính bằng t-att khiến nó trở thành động. Owl sẽ đánh giá biểu thức và đặt nó làm giá trị của thuộc tính.
+
+    // Đoạn mã này định nghĩa một mảng các đối tượng đại diện cho các công việc (tasks). Mỗi đối tượng trong mảng đại diện cho một công việc, bao gồm các thuộc tính sau:
+    //     id: là một số nguyên duy nhất đại diện cho id của công việc.
+    //     text: là một chuỗi đại diện cho nội dung của công việc.
+    //     isCompleted: là một giá trị logic đại diện cho trạng thái hoàn thành của công việc (true nếu đã hoàn thành, false nếu chưa hoàn thành).
+    // Ví dụ trên cho thấy cách lưu trữ thông tin các công việc trong một mảng đối tượng, giúp cho việc quản lý và hiển thị các công việc trở nên dễ dàng hơn trong ứng dụng.
+    tasks = [
+        {
+            id: 1,
+            text: "buy milk",
+            isCompleted: true,
+        },
+        {
+            id: 2,
+            text: "clean house",
+            isCompleted: false,
+        },
+    ];
 }
 
 // Đoạn code mount(Root, document.body); được sử dụng để gắn một thành phần OWL gốc (root component) vào thân của tài liệu HTML.
