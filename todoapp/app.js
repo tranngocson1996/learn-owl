6a1aa595cd995c7b7add50bcba95e66409037d6a
Mountain Tran <tranngocson8296@gmail.com>
2023-03-20 10:41:30 +0700

    12. Filtering tasks-OWL Tutorial: TodoApp

--- todoapp/app.js
+++ todoapp/app.js
@@ -107,20 +107,50 @@ class Task extends Component {
 // -------------------------------------------------------------------------
 class Root extends Component {
     static template = xml/* xml */ `
-        <div class="todo-app">
+    <div class="todo-app">
         <input placeholder="Enter a new task" t-on-keyup="addTask" t-ref="add-input"/>
         <div class="task-list">
-            <t t-foreach="store.tasks" t-as="task" t-key="task.id">
+        <t t-foreach="displayedTasks" t-as="task" t-key="task.id">
             <Task task="task"/>
-            </t>
+        </t>
         </div>
-        </div>`;
+        <div class="task-panel" t-if="store.tasks.length">
+        <div class="task-counter">
+            <t t-esc="displayedTasks.length"/>
+            <t t-if="displayedTasks.length lt store.tasks.length">
+                / <t t-esc="store.tasks.length"/>
+            </t>
+            task(s)
+        </div>
+        <div>
+            <span t-foreach="['all', 'active', 'completed']"
+            t-as="f" t-key="f"
+            t-att-class="{active: filter.value===f}"
+            t-on-click="() => this.setFilter(f)"
+            t-esc="f"/>
+        </div>
+        </div>
+    </div>`;
     static components = { Task };
 
     setup() {
         const inputRef = useRef("add-input");
         onMounted(() => inputRef.el.focus());
         this.store = useStore();
+        this.filter = useState({ value: "all" });
+    }
+
+    get displayedTasks() {
+        const tasks = this.store.tasks;
+        switch (this.filter.value) {
+            case "active": return tasks.filter(t => !t.isCompleted);
+            case "completed": return tasks.filter(t => t.isCompleted);
+            case "all": return tasks;
+        }
+    }
+
+    setFilter(filter) {
+        this.filter.value = filter;
     }
 
     addTask(ev) {
