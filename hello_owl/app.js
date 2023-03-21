034f5696d659b9c3b38f1fdcf09659e03e6ee308
Mountain Tran <tranngocson8296@gmail.com>
2023-03-21 09:54:36 +0700

    With a static server-How to start an Owl project

--- hello_owl/app.js
+++
@@ -1,8 +0,0 @@
-const { Component, mount, xml } = owl;
-
-// Owl Components
-class Root extends Component {
-  static template = xml`<div>Hello Owl</div>`;
-}
-
-mount(Root, document.body);
---
+++ hello_owl/src/main.js
@@ -0,0 +1,5 @@
+const { mount } = owl;
+
+import { Root } from "./root.js";
+
+mount(Root, document.body);
